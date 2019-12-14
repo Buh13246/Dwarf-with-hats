0598ef0fc16fc486859dce9d72c18331419136b9
Philipp Wellner <buh@mintendo-programmer.de>
2019-12-14 18:40:38 +0100

    Small Bugfixes and Code Quality improvements

--- hats.py
+++ hats.py
@@ -1,32 +1,41 @@
-# you can write to stdout for debugging purposes, e.g.
-# print("this is a debug message")
+"""A Dwarf with hats Module.
+
+This module provides a solution(x) function
+which returns a list of ints whether the dwarf has a hat or not.
+"""
+
+__all__ = ['solution']
+__version__ = '0.1'
+__author__ = 'Philipp Wellner'
+
 import threading
 from time import sleep
 # default start can also be empty.
-hats = [1,0,0,1,0,0,0,0,1,0]
+hats = [1, 0, 0, 1, 0, 0, 0, 0, 1, 0]
 
 # prevents that two threads calculates the same result
 calculating_lock = threading.Lock()
 
-def solution(X):
+def solution(x=10):
+    """returns a list of x ints whether the dwarf has a hat or not
+    """
     # last is maybe not correct and is still calculated in another Thread
-    if(len(hats) > X):
-        return hats[:X]
+    if len(hats) > x:
+        return hats[:x]
     # lock if no thread is calculating right now
     while not calculating_lock.acquire(False):
         sleep(0.1)
         # still locked but maybe the other thread did my work
-        if(len(hats) > X):
-            return hats[:X]
-    for current_dwarf in range(len(hats),X+1):
+        if len(hats) > x:
+            return hats[:x]
+    for current_dwarf in range(len(hats), x+1):
         # init default
         hats.append(1)
-        for ii in range(2,current_dwarf+2):
+        for ii in range(2, current_dwarf+2):
             if (current_dwarf + 1) % ii == 0:
                 # using Bit Operations
                 hats[current_dwarf] ^= 1
         # reset all other bits to 0
         hats[current_dwarf] &= 1
     calculating_lock.release()
-    return hats[:X]
-    pass
+    return hats[:x]
--- server.py
+++ server.py
@@ -1,14 +1,21 @@
-import hats
+"""HTTPServer for the dwarfs with hats problem
 
+    Runs a HTTPServer with a ThreadingMixIn socket connection.
+    Provides a simple API with Routes / and /history
+"""
 import json
 import datetime
-import threading, socketserver
+import socketserver
 import os
 import sys
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer
+from http.server import BaseHTTPRequestHandler
 
+import hats
 
 history = {}
+
+SERVER_PORT = 12345
 
 class ServerHandler(BaseHTTPRequestHandler):
     """Handler for the dwarf hats HTTPServer.
@@ -19,7 +26,7 @@ class ServerHandler(BaseHTTPRequestHandler):
     """
     def send_success_headers(self):
         self.send_response(200)
-        self.send_header('Content-type','application/json')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
     def do_GET(self):
         ip_addr = self.client_address[0]
@@ -31,25 +38,25 @@ class ServerHandler(BaseHTTPRequestHandler):
         # get path without GET params
         path_handler = self.path.split('?')[0]
         # GET params
-        if(len(self.path.split('?')) > 1):
+        if len(self.path.split('?')) > 1:
             parameter = {}
             for urlpart in self.path.split('?')[1].split("&"):
                 parameter_splitet = urlpart.split("=")
-                if(len(parameter_splitet) == 2):
+                if len(parameter_splitet) == 2:
                     name_of_parameter = parameter_splitet[0]
-                    # min 1 character
-                    if(len(name_of_parameter) != 0):
-                        parameter[name_of_parameter]=parameter_splitet[1]
+                    # min 1 non whitespace character
+                    if name_of_parameter.strip():
+                        parameter[name_of_parameter.strip()] = parameter_splitet[1]
             print(f'PARAMETER:{parameter}')
 
         if path_handler == "/" or path_handler == "/history":
             self.send_success_headers()
         if path_handler == "/":
             try:
-                X = int(parameter["X"])
+                x = int(parameter["X"])
             except:
-                X=10
-            self.send_hats(X)
+                x = 10
+            self.send_hats(x)
         elif path_handler == "/history":
             self.send_history()
         else:
@@ -58,13 +65,13 @@ class ServerHandler(BaseHTTPRequestHandler):
             self.end_headers()
             return
 
-    def send_hats(self, X = 10):
-        dwarf_hats = hats.solution(X)
+    def send_hats(self, x=10):
+        dwarf_hats = hats.solution(x)
         ip_addr = self.client_address[0]
-        response_obj = {"client_ip_address": ip_addr, "X": X, "dwarf_hat": dwarf_hats}
+        response_obj = {"client_ip_address": ip_addr, "X": x, "dwarf_hat": dwarf_hats}
         history[ip_addr].append(response_obj)
         #write logfile
-        output_file = open(f"logs_history/{ip_addr}_{datetime.datetime.now()}","w")
+        output_file = open(f"logs_history/{ip_addr}_{datetime.datetime.now()}", "w")
         output_file.write(json.dumps(response_obj))
         output_file.close()
         # send json
@@ -72,7 +79,9 @@ class ServerHandler(BaseHTTPRequestHandler):
         return
     def send_history(self):
         ip_addr = self.client_address[0]
-        response_obj = {"client_ip_address": ip_addr, "response_count": len(history[ip_addr]), "responses": history[ip_addr]}
+        response_obj = {"client_ip_address": ip_addr,
+                        "response_count": len(history[ip_addr]),
+                        "responses": history[ip_addr]}
         self.wfile.write(json.dumps(response_obj).encode('utf8'))
 
 # found on StackOverflow
@@ -85,15 +94,18 @@ if not os.path.isdir("./logs_history/"):
     try:
         os.mkdir("./logs_history")
     except Exception as e:
-        print(e.message)
+        print(e)
         sys.exit(1)
 
 # accept from all conections
-dwarf_server = ThreadedHTTPServer(('0.0.0.0',12345), ServerHandler)
+
 try:
+    dwarf_server = ThreadedHTTPServer(('0.0.0.0', SERVER_PORT), ServerHandler)
+    print(f"Server succesfully Listening on Port: {SERVER_PORT}")
     dwarf_server.serve_forever()
 except KeyboardInterrupt:
-	print ('^C received, shutting down the web server')
-	dwarf_server.socket.close()
+    print('^C received, shutting down the web server')
+    dwarf_server.socket.close()
+# maybe address already in use
 except Exception as e:
-    print(e.message)
+    print(e)
